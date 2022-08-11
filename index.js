fe8ec04275e0aa5beb69440bf0800e450c387081
eric jubera <ericjubera@gmail.com>
2022-08-11 11:42:47 -0500

    commit tree 063d379b4964ba2c6cb26417a656d714698113d8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftNumebr=dodger.style.left.replace("px","")
+    const left = parseInt(leftNumebr,10);
+    if (left>0){
+        dodger.style.left=`${left-1}px`
+    }
+}
+
+document.addEventListener("keydown",function(event){
+    if(event.key==="ArrowLeft"){
+        moveDodgerLeft();
+    }
+})
+
+function moveDodgerRight(){
+    const leftNumebr=dodger.style.left.replace("px","")
+    const left=parseInt(leftNumebr,10);
+    if(left>0)[
+        dodger.style.left=`${left+1}px`
+    ]
+
+}
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }})
